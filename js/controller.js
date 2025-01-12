7dc495d48300b864ceade15f1eeb0c49b439e234
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-01-12 18:27:31 +0200

    +

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+import * as Model from "model.js";
+
+window.onload = function () {
+    const getData = Model.getData;
+};
---
+++ js/modal.js
@@ -0,0 +1,15 @@
+let data = {
+    selectedProgramm: 0.1,
+    programms: {
+        base: 0.1,
+        it: 0.047,
+        gov: 0.067,
+        zero: 0.12,
+    },
+};
+
+function getData() {
+    return { ...data };
+}
+
+export { getData };
