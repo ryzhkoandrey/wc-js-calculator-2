6a4a34f4f75de5c3bedaf0aac1d173ea2a7e3838
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-21 06:54:15 +0200

    Update radioPrograms.js

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -18,6 +18,17 @@ function init(getData) {
    radioBtns.forEach(function (radioBtn) {
       radioBtn.addEventListener('change', function () {
          console.log(this);
+         console.log(parseFloat(this.value));
+         console.log(this.id);
+
+         this.dispatchEvent(new CustomEvent('updateForm', {
+            bubbles: true,
+            detail: {
+               selectedProgram: parseFloat(this.value),
+               onUpdate: 'radioProgram',
+               id: this.id,
+            },
+         }));
       });
    });
 }
