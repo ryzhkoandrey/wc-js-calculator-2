d0a06c7acc75219185071af1fb986df39437f77a
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-15 21:20:41 +0200

    5

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,4 +1,5 @@
 function init(getData) {
+   const data = getData();
    const input = document.querySelector('#input-cost');
 
    const settings = {
@@ -7,7 +8,36 @@ function init(getData) {
       delimiter: ' ',
    };
 
-   new Cleave(input, settings);
+   const cleaveInput = new Cleave(input, settings);
+
+   cleaveInput.setRawValue(data.cost);
+
+   input.addEventListener('input', () => {
+      const value = +cleaveInput.getRawValue();
+
+      // Проверка на мин и макс цену
+      if (value < data.minPrice || value > data.maxPrice) {
+         input.closest('.param__details').classList.add('param__details--error');
+      } else {
+         input.closest('.param__details').classList.remove('param__details--error');
+      }
+   });
+
+   input.addEventListener('change', () => {
+      const value = +cleaveInput.getRawValue();
+      const minPrice = data.minPrice;
+      const maxPrice = data.maxPrice;
+
+      if (value < minPrice) {
+         input.closest('.param__details').classList.remove('param__details--error');
+         cleaveInput.setRawValue(minPrice);
+      }
+
+      if (value > maxPrice) {
+         input.closest('.param__details').classList.remove('param__details--error');
+         cleaveInput.setRawValue(maxPrice);
+      }
+   });
 }
 
 export default init;
